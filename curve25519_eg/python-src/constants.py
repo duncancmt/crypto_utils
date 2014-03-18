f3c1aae3538055696ed59383196169e375cc2f5e
Duncan Townsend <duncant@mit.edu>
2014-03-18 12:47:44 -0400

    Made use of newly exposed field arithmetic routines in the Curve25519Element methods

--- curve25519_eg/python-src/constants.py
+++ curve25519_eg/python-src/constants.py
@@ -1,47 +1,10 @@
-# TODO: make use of the fast library functions for modular multiplication and inversion
-
 import _curve25519
 from intbytes import int2bytes, bytes2int
 from numbers import Integral
 
-try:
-    from gmpy2 import mpz, invert
-    mpz_type = type(mpz())
-    has_gmpy = True
-except ImportError:
-    try:
-        from gmpy import mpz, invert
-        mpz_type = type(mpz())
-        has_gmpy = True
-    except ImportError:
-        import warnings
-        warnings.warn('Not having gmpy2 or gmpy makes this at least 10x slower')
-        mpz_type = Integral
-        has_gmpy = False
-
-if not has_gmpy:
-    def egcd(a, b):
-        """The Extended Euclidean Algorithm
-        In addition to finding the greatest common divisor (GCD) of the
-        arguments, also find and return the coefficients of the linear
-        combination that results in the GCD.
-        """
-        if a == 0:
-            return (b, 0, 1)
-        else:
-            g, y, x = egcd(b % a, a)
-            return (g, x - (b // a) * y, y)
-    def invert(a, m):
-        """Return the multiplicative inverse of a, mod m"""
-        g, x, y = egcd(a, m)
-        if g != 1:
-            raise ValueError('modular inverse does not exist')
-        else:
-            return x % m
-
 class Curve25519Point(bytes):
     def __new__(cls, x):
-        if isinstance(x, (Integral, mpz_type)):
+        if isinstance(x, Integral):
             x = int2bytes(x, length=32, endian='little')
         if not isinstance(x, bytes):
             raise TypeError("Can only instantiate Curve25519Point instances from integers or bytes")
@@ -55,7 +18,7 @@ class Curve25519Point(bytes):
 
 class Curve25519Element(bytes):
     def __new__(cls, x):
-        if isinstance(x, (Integral, mpz_type)):
+        if isinstance(x, Integral):
             x = int2bytes(x % bytes2int(p, endian='little'),
                           length=32,
                           endian='little')
@@ -69,12 +32,11 @@ class Curve25519Element(bytes):
     def __mul__(self, other):
         if not isinstance(other, Curve25519Element):
             raise TypeError("Multiplication is only defined on Curve25519Element's")
-        return Curve25519Element(bytes2int(self, endian='little') * bytes2int(other, endian='little'))
+        return Curve25519Element(_curve25519.mul(self, other))
     def __div__(self, other):
         if not isinstance(other, Curve25519Element):
             raise TypeError("Division is only defined on Curve25519Element's")
-        return Curve25519Element(bytes2int(self, endian='little')
-                                 * invert(bytes2int(other, endian='little'), bytes2int(p, endian='little')))
+        return Curve25519Element(_curve25519.mul(self, _curve25519.recip(other)))
 
 p = 2**255 - 19
 base = 9
