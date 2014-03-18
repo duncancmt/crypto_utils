9529f01ae6a6808fd3fd35427abc67000f063062
Duncan Townsend <duncant@mit.edu>
2014-03-18 14:05:23 -0400

    Removed fluff from curve25519-donna.h

--- curve25519/curve25519-donna.h
+++ curve25519/curve25519-donna.h
@@ -2,13 +2,12 @@
 #define CURVE25519_DONNA
 
 typedef uint8_t u8;
-typedef int32_t s32;
 typedef int64_t limb;
 
-extern int curve25519_donna(u8 *, const u8 *, const u8 *);
-extern void fexpand(limb *, const u8 *);
 extern void fmul(limb *, const limb *, const limb *);
+extern void fexpand(limb *, const u8 *);
 extern void fcontract(u8 *, limb *);
 extern void crecip(limb *, const limb *);
+extern int curve25519_donna(u8 *, const u8 *, const u8 *);
 
 #endif
