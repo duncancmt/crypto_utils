f389d89247da894b8f26ee11afc1ccd90b6e7437
Duncan Townsend <duncant@mit.edu>
2014-03-18 23:35:04 -0400

    Moved constants.py to curve.py to reflect its expanded contents Now we use a namedtuple class to represent ciphertexts

--- curve25519/python-src/elgamal.py
+++ curve25519/python-src/elgamal.py
@@ -1,7 +1,8 @@
 import random
 from operator import or_
 from itertools import imap
-from constants import *
+from collections import namedtuple
+from curve import *
 from intbytes import int2bytes, bytes2int, encode_varint, decode_varint
 from immutable import ImmutableEnforcerMeta
 from util import secure_compare
@@ -30,11 +31,14 @@ def element_to_message(element):
     element = element[:length]
     return element
 
+Curve25519CipherText = namedtuple("Curve25519CipherText", ["locks", "box"])
+
 class Curve25519ElGamalKey(object):
     # TODO: allow hashing of the shared secret before multiplication
-    # TODO: use namedtuples for ciphertexts
     """Implements encoding-free multiplicative ElGamal encryption as described in
-    Encoding-Free ElGamal Encryption Without Random Oracles"""
+    Encoding-Free ElGamal Encryption Without Random Oracles by Chevallier-Mames et.al.
+    over Curve25519 as described in
+    Curve25519: new Diffie-Hellman speed records by Bernstein"""
     __metaclass__ = ImmutableEnforcerMeta
     def __init__(self, _pubkey=None, _seckey=None):
         if _pubkey is not None:
@@ -56,11 +60,13 @@ class Curve25519ElGamalKey(object):
         c2 = curve(r, self.pubkey)
         # c3 = message*c2 = message * g^(seckey * r)
         c3 = message * Curve25519Element(c2)
-        return (c1, c3)
+        return Curve25519CipherText(locks={self.pubkey:c1}, box=c3)
     
     def decrypt(self, message):
         # TODO: add alternate second argument
-        c1, d = message
+        # TODO: type check
+        c1 = message.locks[self.pubkey]
+        d = message.box
         # c = c1^seckey = g^(seckey * r)
         c = curve(self.seckey, c1)
         # m = d/c = original * g^(seckey * r) * g^(seckey * r)^-1 = original
@@ -105,4 +111,4 @@ class Curve25519ElGamalKey(object):
         self._seckey = seckey
 
 
-__all__ = ['Curve25519ElGamalKey']
+__all__ = ['Curve25519ElGamalKey', 'Curve25519CipherText']
