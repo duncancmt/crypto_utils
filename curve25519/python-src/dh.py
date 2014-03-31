7c8696a4ae1df75f56b7f491e5f61bc0ac813ac2
Duncan Townsend <duncant@mit.edu>
2014-03-31 15:30:29 -0400

    Added dh.py with DHKey class implementing elliptic curve Diffie-Hellman

--- curve25519/python-src/dh.py
+++ curve25519/python-src/dh.py
@@ -1,53 +1,11 @@
-import random
-from curve import *
+from key import Key
+from curve import curve
 
-class Curve25519DHKey(object):
-    __metaclass__ = ImmutableEnforcerMeta
+class DHKey(Key):
+    """Implements elliptic curve Diffie-Hellman key exchange over Curve25519"""
 
     def shared_secret(self, other):
-        if not isinstance(other, Curve25519DHKey):
-            raise TypeError("Can only calculate the shared secret with another Curve25519DHKey instance")
+        if not isinstance(other, DHKey):
+            raise TypeError("Argument must be a DHKey instance")
         return curve(self.seckey, other.pubkey)
-
-    @classmethod
-    def generate(cls, random=random):
-        return cls.from_seckey(random.getrandbits(32*8))
-    @classmethod
-    def from_pubkey(cls, pubkey):
-        if reduce(or_, imap(lambda bad: secure_compare(pubkey, bad), bad_public_keys)):
-            raise RuntimeError("Tried to instantiate Curve25519DHKey with a bad public key")
-        else:
-            return cls(_pubkey=Curve25519Point(pubkey))
-    @classmethod
-    def from_seckey(cls, seckey):
-        return cls(_seckey=Curve25519SubElement(seckey))
-    @classmethod
-    def from_privkey(cls, privkey):
-        return cls.from_seckey(privkey)
-
-    ### Aliases for immutable state
-    @property
-    def pubkey(self):
-        try:
-            return self._pubkey
-        except AttributeError:
-            self.pubkey = curve(self.seckey, base)
-            return self._pubkey
-    @pubkey.setter
-    def pubkey(self, pubkey):
-        assert isinstance(pubkey, Curve25519Point)
-        self._pubkey = pubkey
-
-    @property
-    def seckey(self):
-        return self._seckey
-    @seckey.setter
-    def seckey(self, seckey):
-        assert isinstance(seckey, Curve25519SubElement)
-        self._seckey = seckey
-    @property
-    def privkey(self):
-        return self.seckey
-    @privkey.setter
-    def privkey(self, privkey):
-        self.seckey = privkey
+__all__ = ["DHKey"]
