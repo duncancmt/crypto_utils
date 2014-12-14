440f6bb17c8a188e759b18afb7d072dff18ae78f
Duncan Townsend <duncant@mit.edu>
2014-12-13 20:47:38 -0500

    Python's built-in compare_digest is crappy when strings are of different lengths. My secure_compare handles them slightly better, but there's still a slight problem with cache timing (see comments).

--- util.py
+++ util.py
@@ -1,33 +1,31 @@
-try:
-    from hmac import compare_digest as secure_compare
-except ImportError:
-    from operator import or_, xor
-    from itertools import imap
-    
-    def secure_compare(a,b):
-        """Return 'a == b', but try not to leak timing information about the
-        arguments. In the event that the length of the two strings are not
-        equal, we leak the length of the right argument, b.
-        """
-        error = TypeError('Arguments must be bytes with the same length')
-        retval = True
-        
-        if not isinstance(a, bytes):
-            retval = error
-        if not isinstance(b, bytes):
-            retval = error
-        if isinstance(retval, TypeError):
-            raise retval
-    
-        if len(a) == len(b):
-            a,b = a[::],b[::]
-            retval &= True
-        if len(a) != len(b):
-            a,b = b[::],b[::]
-            retval &= False
+from itertools import imap
+from operator import or_, xor
+def secure_compare(a,b):
+    """Return 'a == b', but try not to leak timing information about the
+    arguments. In the event that the length of the two strings are not
+    equal, we leak the length of the right argument, b.
+    """
+    retval = True
 
-        retval &= (reduce(or_, imap(xor, imap(ord, a),
-                                         imap(ord, b)), 0) == 0)
-        return retval
+    if not (isinstance(a, bytes) & isinstance(b, bytes)):
+        raise TypeError('Arguments must be bytes')
+
+    # copy b to a if the lengths of a and b are unequal
+    retval &= len(a) == len(b)
+    # some gymnastics we have to do because of small integer caching
+    new_a = [None] * ( len(a) & -retval\
+                       | len(b) & ~-retval )
+    for i in xrange(len(new_a)):
+        # doing `% len(a)' here suppresses errors, but may leak information about
+        # the length of a through cache timing
+        new_a[i] = long(ord(a[i % len(a)])) & -retval \
+                   | long(ord(b[i])) & ~-retval
+    a = ''.join(imap(chr, new_a))
+    del new_a
+    del i
+
+    retval &= (reduce(or_, imap(xor, imap(ord, a),
+                                     imap(ord, b)), 0) == 0)
+    return retval
 
 __all__ = ['secure_compare']
